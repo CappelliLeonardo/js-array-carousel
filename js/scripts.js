7b66a00029d4c53e982959f09b71fe6b67f33871
CappelliLeonardo <l.cappelli1995@gmail.com>
2023-12-11 14:40:27 +0100

    refactoring esercizio

--- js/scripts.js
+++ js/scripts.js
@@ -1,108 +1,78 @@
-// Consegna:
-// Dato un array contenente una lista di cinque immagini,
-// creare un carosello come nello screenshot allegato.
-
-// MILESTONE 1
-// Per prima cosa, creiamo il markup statico:
-// costruiamo il container e inseriamo un'immagine grande al centro: avremo così
-// la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
-
-// MILESTONE 2
-// Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini
-// dinamicamente servendoci dell'array fornito e un semplice
-// ciclo for che concatena un template literal.
-// Tutte le immagini saranno nascoste, tranne la prima, che avrà una
-// classe specifica che la renderà visibile.
-// Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1,
-// ma costruito dinamicamente attraverso JavaScript.
-
-// MILESTONE 3
-// Al click dell'utente sulle frecce, il programma cambierà l'immagine attiva,
-// che quindi verrà visualizzata al posto della precedente.
-// Prima di partire a scrivere codice:
-// Non lasciamoci spaventare dalla complessità apparente dell'esercizio, ma analizziamo prima,
-// come abbiamo fatto sempre, cosa ci potrebbe aspettare. Abbiamo completato ormai
-// da qualche giorno la sessione HTML e CSS, se non ci ricordiamo qualcosa andiamo pure a
-// riguardare alcuni argomenti. Non dedichiamo però al ripasso più di una mezz'ora, così da non
-// perdere di vista il focus dell'esercizio.
 
 
-// Consigli del giorno:
+/* Inseriamo le singole img in delle variabili con un template Literl con il quale 
+integriamo la variabile della classe */
 
-// 1. Costruiamo del carosello una versione statica contenente solamente un'immagine.
-// Di questa versione statica al momento opportuno commenteremo (oscureremo) alcuni elementi per
-// poterli riprodurre dinamicamente in js. Potremo quindi usarli come "template".
+const img1 =  document.querySelector('.canvas').innerHTML = `
+<img src="img/01.webp" alt="Spiderman">
+`;
 
-// 2. Scriviamo sempre prima per punti il nostro algoritmo in italiano per capire cosa vogliamo fare
+const img2 =  document.querySelector('.canvas').innerHTML = `
+<img src="img/02.webp" alt="Ratchet&Clanck">
+`;
 
-// 3. Al momento giusto (ihihhi starà a voi capire quale) rispondete a questa
-// domanda: "Quanti cicli servono?"
+const img3 =  document.querySelector('.canvas').innerHTML = `
+<img src="img/03.webp" alt="Fortnite">
+`;
 
+const img4 =  document.querySelector('.canvas').innerHTML = `
+<img src="img/04.webp" alt="Stray">
+`;
 
- 
-
-// const myImages = [
-//     'item_one',
-//     'item_two',
-//     'item_three',
-//     'item_four',
-//     'item_five'
-// ];
-
-
-// document.getElementById('container').append(myImages);
+const img5 =  document.querySelector('.canvas').innerHTML = `
+<img src="img/05.webp" alt="Avengers">
+`;
 
 
-// const img_two = document.createElement("div");
-// const img_three = document.createElement("div");
-// const img_four = document.createElement("div");
-// const img_five = document.createElement("div");
+// array delle img da inserire, posta in variabili
+const imgList = [ 
+     ` ${img1} ` ,
+     ` ${img2} ` ,
+     ` ${img3} ` ,
+     ` ${img4} ` ,
+     ` ${img5} ` 
+      ];
 
-// const images = [
-//     './img/01.webp',
-//     './img/02.webp',
-//     './img/03.webp',
-//     './img/04.webp',
-//     './img/05.webp'
-// ];
-
-
-const images = [
-    './img/01.webp',
-    './img/02.webp',
-    './img/03.webp',
-    './img/04.webp',
-    './img/05.webp'
-];
-const imagesContainer = document.getElementById('container');
-
-for(let i = 0; i < images.length; i++) {
+     
+document.querySelector('.canvas').innerHTML = (imgList[0]);
 
 
-    // const itemDiv = document.createElement('div'); // <div
-    // itemDiv.setAttribute('class', 'items js'); //<div class="items"
+let counter = 0 ;
 
-    // const image =  document.createElement('img');
-    // image.setAttribute('src', images[i]);
-    // // image.src = images[i];
 
-    // itemDiv.appendChild(image);
-    // imagesContainer.appendChild(itemDiv);
+// associamo una variabile ai bottoni
+const buttonRight = document.getElementById('button-right');
+const buttonLeft = document.getElementById('button-left');
 
-    imagesContainer.innerHTML += `
-        <div class="items">
-            <img src="${images[i]}">
-        </div>
-    `
-}
 
-const button = document.getElementById('up-button');
-button.addEventListener('click', function(){
+
+buttonRight.addEventListener('click', function(){
+
+      
+    if (counter < imgList.length -1){
+
+        counter += 1;
+
+        console.log(counter);
+
+        document.querySelector('.canvas').innerHTML = imgList[counter];
+    }
+    
+
+})
+
+
+buttonLeft.addEventListener('click', function(){
     
     
-}
-);
-// SE viene cliccato aumenta una unità
-// altrimenti niente
+        if ((counter !== imgList.length) && (counter !== 0)){
 
-// if se aumenta di una unita allora metti d-none a img [i]
+            counter -= 1
+            console.log(counter)
+
+            document.querySelector('.canvas').innerHTML = (imgList[counter])
+
+        }
+
+    
+}) 
